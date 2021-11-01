4b5ecd67afea5cacb0d53f36485e9db27bae0f26
Ron Liebregts <ronlieb@hotmail.com>
2021-11-01 11:27:29 +0100

    Initial commit

---
+++ OSRS Groceries/Models/Grocerylist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OSRS_Groceries.Models
+{
+    public class Grocerylist
+    {
+        public virtual User User { get; set; }
+        public virtual ICollection<Item> Items { get; set; }
+    }
+}
---
+++ OSRS Groceries/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OSRS_Groceries.Models
+{
+    public class Item
+    {
+        [Key]
+        [Required]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Value { get; set; }
+    }
+}
