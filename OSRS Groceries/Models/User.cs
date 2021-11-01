039c517aa4e7c381e85a965ba2e4423b55d5d1c8
Ron Liebregts <ronlieb@hotmail.com>
2021-11-01 14:17:38 +0100

    DBContext

--- OSRS Groceries/Models/User.cs
+++ OSRS Groceries/Models/User.cs
@@ -9,9 +9,10 @@ namespace OSRS_Groceries.Models
     public class User
     {
         [Key]
-        [Required]
         public int ID { get; set; }
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Password { get; set; }
     }
